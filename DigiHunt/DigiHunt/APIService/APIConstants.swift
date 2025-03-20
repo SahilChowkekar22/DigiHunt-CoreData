743c70a22737ac718e3d4286b6573b5afb426090
SahilChowkekar22 <Sahil.Chowkekar@techconsulting.tech>
2025-03-20 10:10:36 -0400

    Add files via upload

---
+++ DigiHunt/DigiHunt/APIService/APIConstants.swift
@@ -0,0 +1,12 @@
+//
+//  APIConstants.swift
+//  DigiHunt
+//
+//  Created by Sahil ChowKekar on 3/3/25.
+//
+
+import Foundation
+
+struct APIConstants {
+    static let DigimonEndPoint = "https://digimon-api.vercel.app/api/digimon"
+}
